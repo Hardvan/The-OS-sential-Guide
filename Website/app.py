0bc02b84bf6fb20d16f04b966979de02990fd12a
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-26 10:34:24 +0530

    Add FCFS algorithm

--- Website/app.py
+++ Website/app.py
@@ -26,19 +26,51 @@ def cpu():
     """
 
     if request.method == 'POST':
-        # Get form data
-        data = []
-        for i in range(1, 5):
-            arrival_time = request.form.get(f'arrival_time_{i}')
-            burst_time = request.form.get(f'burst_time_{i}')
-            priority = request.form.get(f'priority_{i}')
-            data.append({'arrival_time': arrival_time,
-                        'burst_time': burst_time, 'priority': priority})
-        # Do something with the data
-        print(data)
 
-        # TODO: Redirect to cpu.html with the data
-        return "Form submitted successfully!"
+        # Converting form data to a list of tuples
+        processes = []
+        for i in range(1, 5):
+            pid = f'P{i}'
+            arrival_time = int(request.form.get(f'arrival_time_{i}'))
+            burst_time = int(request.form.get(f'burst_time_{i}'))
+            priority = int(request.form.get(f'priority_{i}'))
+            processes.append((pid, arrival_time, burst_time, priority))
+
+        print(processes)  # Testing
+
+        # ? FCFS Algorithm
+        processes.sort(key=lambda x: x[1])  # Sort by arrival time
+        completion_times = []
+        waiting_times = []
+        turnaround_times = []
+        current_time = 0
+        for process in processes:
+            pid, arrival_time, burst_time, priority = process
+
+            current_time = max(current_time, arrival_time)
+
+            completion_time = current_time + burst_time
+            waiting_time = current_time - arrival_time
+            turnaround_time = waiting_time + burst_time
+
+            completion_times.append(completion_time)
+            waiting_times.append(waiting_time)
+            turnaround_times.append(turnaround_time)
+
+            current_time = completion_time
+
+        average_waiting_time = sum(waiting_times) / len(waiting_times)
+        average_turnaround_time = sum(turnaround_times) / len(turnaround_times)
+
+        FCFS_result = {}
+        FCFS_result['processes'] = processes
+        FCFS_result['completion_times'] = completion_times
+        FCFS_result['waiting_times'] = waiting_times
+        FCFS_result['turnaround_times'] = turnaround_times
+        FCFS_result['average_waiting_time'] = average_waiting_time
+        FCFS_result['average_turnaround_time'] = average_turnaround_time
+
+        return render_template("cpu_result.html", FCFS_result=FCFS_result)
 
     return render_template("cpu.html")
 
