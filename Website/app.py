1bb28202658966ac0b11b4a4ba31856101326782
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-26 10:14:09 +0530

    Add form for Process AT, BT, Priority and receive

--- Website/app.py
+++ Website/app.py
@@ -1,7 +1,7 @@
 # cd to the folder where the app.py file is located
 # run the command: python app.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,13 +17,29 @@ def index():
     return render_template("home.html")
 
 
-@app.route('/cpu')
+@app.route('/cpu', methods=['GET', 'POST'])
 def cpu():
     """CPU Scheduling Page
 
     Returns:
         [html]: [CPU Scheduling Page]
     """
+
+    if request.method == 'POST':
+        # Get form data
+        data = []
+        for i in range(1, 5):
+            arrival_time = request.form.get(f'arrival_time_{i}')
+            burst_time = request.form.get(f'burst_time_{i}')
+            priority = request.form.get(f'priority_{i}')
+            data.append({'arrival_time': arrival_time,
+                        'burst_time': burst_time, 'priority': priority})
+        # Do something with the data
+        print(data)
+
+        # TODO: Redirect to cpu.html with the data
+        return "Form submitted successfully!"
+
     return render_template("cpu.html")
 
 
