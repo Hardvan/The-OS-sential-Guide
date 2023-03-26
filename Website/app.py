c1c0a67a5e4ad585b9d40862387d6a9a5c7d7ff3
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-26 10:50:33 +0530

    Add SJF

--- Website/app.py
+++ Website/app.py
@@ -2,6 +2,7 @@
 # run the command: python app.py
 
 from flask import Flask, render_template, request
+import copy
 
 app = Flask(__name__)
 
@@ -25,25 +26,16 @@ def cpu():
         [html]: [CPU Scheduling Page]
     """
 
-    if request.method == 'POST':
+    def fcfs(processes):
 
-        # Converting form data to a list of tuples
-        processes = []
-        for i in range(1, 5):
-            pid = f'P{i}'
-            arrival_time = int(request.form.get(f'arrival_time_{i}'))
-            burst_time = int(request.form.get(f'burst_time_{i}'))
-            priority = int(request.form.get(f'priority_{i}'))
-            processes.append((pid, arrival_time, burst_time, priority))
+        processes = copy.deepcopy(processes)
 
-        print(processes)  # Testing
-
-        # ? FCFS Algorithm
         processes.sort(key=lambda x: x[1])  # Sort by arrival time
         completion_times = []
         waiting_times = []
         turnaround_times = []
         current_time = 0
+
         for process in processes:
             pid, arrival_time, burst_time, priority = process
 
@@ -62,17 +54,77 @@ def cpu():
         average_waiting_time = sum(waiting_times) / len(waiting_times)
         average_turnaround_time = sum(turnaround_times) / len(turnaround_times)
 
-        FCFS_result = {}
-        FCFS_result['processes'] = processes
-        FCFS_result['completion_times'] = completion_times
-        FCFS_result['waiting_times'] = waiting_times
-        FCFS_result['turnaround_times'] = turnaround_times
-        FCFS_result['average_waiting_time'] = average_waiting_time
-        FCFS_result['average_turnaround_time'] = average_turnaround_time
+        result = {}
+        result['processes'] = processes
+        result['completion_times'] = completion_times
+        result['waiting_times'] = waiting_times
+        result['turnaround_times'] = turnaround_times
+        result['average_waiting_time'] = average_waiting_time
+        result['average_turnaround_time'] = average_turnaround_time
 
-        return render_template("cpu_result.html", FCFS_result=FCFS_result)
+        return result
 
-    return render_template("cpu.html")
+    def sjf(processes):
+
+        processes = copy.deepcopy(processes)
+
+        processes_sorted = sorted(
+            processes, key=lambda x: x[2])  # Sort by burst time
+        completion_times = []
+        waiting_times = []
+        turnaround_times = []
+        current_time = 0
+
+        while processes_sorted:
+            # Select the process with the shortest burst time
+            next_process = processes_sorted.pop(0)
+            pid, arrival_time, burst_time, priority = next_process
+
+            current_time = max(current_time, arrival_time)
+
+            completion_time = current_time + burst_time
+            waiting_time = current_time - arrival_time
+            turnaround_time = waiting_time + burst_time
+
+            completion_times.append(completion_time)
+            waiting_times.append(waiting_time)
+            turnaround_times.append(turnaround_time)
+
+            current_time = completion_time
+
+        average_waiting_time = sum(waiting_times) / len(waiting_times)
+        average_turnaround_time = sum(turnaround_times) / len(turnaround_times)
+
+        result = {}
+        result['processes'] = processes_sorted
+        result['completion_times'] = completion_times
+        result['waiting_times'] = waiting_times
+        result['turnaround_times'] = turnaround_times
+        result['average_waiting_time'] = average_waiting_time
+        result['average_turnaround_time'] = average_turnaround_time
+
+        return result
+
+    if request.method == 'POST':
+
+        # Converting form data to a list of tuples
+        processes = []
+        for i in range(1, 5):
+            pid = f'P{i}'
+            arrival_time = int(request.form.get(f'arrival_time_{i}'))
+            burst_time = int(request.form.get(f'burst_time_{i}'))
+            priority = int(request.form.get(f'priority_{i}'))
+            processes.append((pid, arrival_time, burst_time, priority))
+
+        # Run FCFS algorithm
+        FCFS_result = fcfs(processes)
+
+        # Run SJF algorithm
+        SJF_result = sjf(processes)
+
+        return render_template('cpu_result.html', FCFS_result=FCFS_result, SJF_result=SJF_result)
+
+    return render_template('cpu.html')
 
 
 if __name__ == "__main__":
